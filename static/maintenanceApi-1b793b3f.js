f012048495da910dd88fd6f49769ebeb43c29b09
tzx-sujie <39177106+tzx-sujie@users.noreply.github.com>
2023-06-14 16:26:25 +0000

    Deploying to gh-pages from main @ b417a9e79057f62ee5bdc83e4b8fbd673e4d4fb0 🚀

---
+++ static/maintenanceApi-1b793b3f.js
@@ -0,0 +1,1 @@
+import{C as s}from"./Loading-ae4e87fc.js";class r extends s{constructor(){super("/api/sale/v1/sysSaleOrder")}}export{r as M};
---
+++ static/materialArchivingApi-e2c8239c.js
@@ -0,0 +1,1 @@
+import{C as s}from"./Loading-ae4e87fc.js";class a extends s{constructor(){super("/api/sale/v1/sysMaterialFile")}}export{a as M};
